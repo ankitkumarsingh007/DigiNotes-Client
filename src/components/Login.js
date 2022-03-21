bf878d7d0bf25b79bdd5b82b17b62dc592e93a8a
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-03-21 22:12:16 +0530

    first

---
+++ src/components/Login.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  let history = useHistory();
+  const { showAlert } = props;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // save the auth token and redirect
+      localStorage.setItem("token", json.authtoken);
+      history.push("/");
+      props.showAlert("Successfully logged in", "success");
+    } else {
+      props.showAlert("Invalid details", "danger");
+    }
+  };
+
+  const onchange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <section className="vh-90 ">
+        <div className="container py-5 h-100">
+          <div className="row d-flex align-items-center justify-content-center h-100">
+            <div className="col-md-8 col-lg-7 col-xl-6">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                className="img-fluid my-2"
+                alt="Phone image"
+              />
+            </div>
+            <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+              <form>
+                {/* Email input */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={credentials.email}
+                    aria-describedby="emailHelp"
+                    onChange={onchange}
+                  />
+                  <label className="form-label" htmlFor="form1Example13">
+                    Email address
+                  </label>
+                </div>
+                {/* Password input */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={credentials.password}
+                    name="password"
+                    onChange={onchange}
+                  />
+                  <label className="form-label" htmlFor="form1Example23">
+                    Password
+                  </label>
+                </div>
+
+                {/* Submit button */}
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg btn-block"
+                  onClick={handleSubmit}
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/Notes.js
@@ -0,0 +1,171 @@
+import react, { useContext, useEffect, useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
+import noteContext from "../context/notes/noteContext";
+import { AddNote } from "./AddNote";
+import { Noteitem } from "./Noteitem";
+
+export const Notes = (props) => {
+  const context = useContext(noteContext);
+  let history = useHistory();
+  const { notes, getNotes, editNote } = context;
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      getNotes();
+    } else {
+      history.push("/login");
+    }
+  }, []);
+  const { showAlert } = props;
+
+  const ref = useRef(null);
+  const refClose = useRef(null);
+  const [note, setNote] = useState({
+    id: "",
+    etitle: "",
+    edescription: "",
+    etag: "",
+  });
+
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({
+      id: currentNote._id,
+      etitle: currentNote.title,
+      edescription: currentNote.description,
+      etag: currentNote.tag,
+    });
+  };
+
+  const handleclick = (e) => {
+    editNote(note.id, note.etitle, note.edescription, note.etag);
+    refClose.current.click();
+    props.showAlert("Updated successfully", "success");
+  };
+
+  const onchange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <AddNote showAlert={props.showAlert} />
+      <button
+        type="button"
+        ref={ref}
+        className="btn btn-primary d-none"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Edit Note
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              {/*form for editing note */}
+              <form className="my-3">
+                <div className="mb-3">
+                  <label htmlFor="etitle" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    value={note.etitle}
+                    className="form-control"
+                    id="etitle"
+                    name="etitle"
+                    aria-describedby="emailHelp"
+                    onChange={onchange}
+                    minLength={5}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="edescription" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    value={note.edescription}
+                    className="form-control"
+                    id="edescription"
+                    name="edescription"
+                    onChange={onchange}
+                    minLength={5}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="etag" className="form-label">
+                    Tag
+                  </label>
+                  <input
+                    type="text"
+                    value={note.etag}
+                    className="form-control"
+                    id="etag"
+                    name="etag"
+                    onChange={onchange}
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                ref={refClose}
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                onClick={handleclick}
+                className="btn btn-primary"
+                disabled={
+                  note.etitle.length < 5 || note.edescription.length < 5
+                }
+              >
+                Update Note
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row my-3">
+        <h2>Your Note</h2>
+        <div className="container">
+          {notes.length === 0 && "No notes to display"}
+        </div>
+        {notes.map((note) => {
+          return (
+            <Noteitem
+              key={note._id}
+              updateNote={updateNote}
+              showAlert={props.showAlert}
+              note={note}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
