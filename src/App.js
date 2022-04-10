6d2a59de5960c8fc69258c94e7bd85050e6ea715
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-04-10 19:55:09 +0530

    Footer Adjusted

--- src/App.js
+++ src/App.js
@@ -24,26 +24,28 @@ function App() {
   return (
     <>
       <NoteState>
-        <Router>
-          <Navbar />
-          <Alert alert={alert} />
-          <div className="container my-2">
-            <Switch>
-              <Route exact path="/">
-                <Home showAlert={showAlert} />
-              </Route>
-              <Route exact path="/about">
-                <About />
-              </Route>
-              <Route exact path="/login">
-                <Login showAlert={showAlert} />
-              </Route>
-              <Route exact path="/signup">
-                <Signup showAlert={showAlert} />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+        <div style={{ minHeight: "95vh" }}>
+          <Router>
+            <Navbar />
+            <Alert alert={alert} />
+            <div className="container my-2">
+              <Switch>
+                <Route exact path="/">
+                  <Home showAlert={showAlert} />
+                </Route>
+                <Route exact path="/about">
+                  <About />
+                </Route>
+                <Route exact path="/login">
+                  <Login showAlert={showAlert} />
+                </Route>
+                <Route exact path="/signup">
+                  <Signup showAlert={showAlert} />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </div>
         <p className="text-center fw-bold">
           Copyright ©{" "}
           <a
