e0b2632f8690ca8b93884512243dbc649f3bbdb9
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-07-05 13:38:24 +0530

    Add files via upload

---
+++ src/components/TextForm.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div className="container">
+        <h2 className="mb-4">{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-dark mx-1 my-1"
+          onClick={handleUpClick}
+        >
+          Convert to Uppercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-dark mx-1 my-1"
+          onClick={handleLoClick}
+        >
+          Convert to Lowercase
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-dark mx-1 my-1"
+          onClick={handleExtraSpaces}
+        >
+          Remove Extra Spaces
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-dark mx-1 my-1"
+          onClick={handleClearClick}
+        >
+          Clear Text
+        </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-dark mx-1 my-1"
+          onClick={handleCopy}
+        >
+          Copy Text
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>
+          {
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(/\s+/).filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          Minutes to read
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
+      </div>
+    </>
+  );
+}
