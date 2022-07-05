458451b9fc96b4f361eaf11541e58de440f54810
Ankit kumar singh <86914556+ankitkumarsingh007@users.noreply.github.com>
2022-07-05 14:54:27 +0530

    Delete src directory

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import { Notes } from "./Notes";
-export const Home = (props) => {
-  const { showAlert } = props;
-  return (
-    <div>
-      <Notes showAlert={showAlert} />
-    </div>
-  );
-};
